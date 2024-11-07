2c7362b6a3c4b356c943a3c2638c3c21cc8424c8
AkshayN01 <nambly77@gmail.com>
2024-11-07 17:33:03 +0000

    Added database model for Payment Service

---
+++ src/CKMS/CKMS.Contracts/DBModels/PaymentService/Payment.cs
@@ -0,0 +1,39 @@
+﻿using CKMS.Contracts.DBModels.OrderService;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKMS.Contracts.DBModels.PaymentService
+{
+    public class Payment
+    {
+        [Key]
+        public Guid PaymentId { get; set; }
+        [Required]
+        public Guid OrderId { get; set; } = Guid.Empty!; //Foreign key
+        [Required]
+        public double Amount { get; set; }
+        [Required]
+        public int PaymentMethod { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public int PaymentStatus { get; set; }
+        public Order Order { get; set; } = null!; //Required
+    }
+
+    public enum PaymentMethod
+    {
+        CashOnDelivery = 0,
+        Online = 1
+    }
+
+    public enum PaymentStatus
+    {
+        Pending = 0,
+        Paid = 1,
+        Failed = 2,
+        Canceled = 3
+    }
+}
