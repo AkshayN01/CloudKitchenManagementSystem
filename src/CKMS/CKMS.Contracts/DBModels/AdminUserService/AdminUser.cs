9e6af5f57093d4a18adc8ce0d9f2ef243c455960
AkshayN01 <nambly77@gmail.com>
2024-11-07 17:18:14 +0000

    Added Admin Service Database models

---
+++ src/CKMS/CKMS.Contracts/DBModels/AdminUserService/AdminUser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CKMS.Contracts.DBModels.AdminUserService
+{
+    public class AdminUser
+    {
+        [Key]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        public string PasswordHash { get; set; } = string.Empty;
+        [Required]
+        public string EmailId { get; set; } = string.Empty;
+        [Required]
+        public string FullName { get; set; } = string.Empty;
+        public int RoleId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public Guid KitchenId { get; set; } //Required Foreign Key
+        public Kitchen Kitchen { get; set; } = null!; //Required
+    }
+
+    public enum Role
+    {
+        SuperAdmin = 0,
+        Admin = 1,
+        KitchenStaff = 2,
+        DeliveryPartner = 3
+    }
+}
---
+++ src/CKMS/CKMS.Contracts/DBModels/AdminUserService/Kitchen.cs
@@ -0,0 +1,16 @@
+﻿namespace CKMS.Contracts.DBModels.AdminUserService
+{
+    public class Kitchen
+    {
+        public Guid KitchenId { get; set; }
+        public string KitchenName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Region { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+        public ICollection<AdminUser> Users { get; set; } = new List<AdminUser>();
+    }
+}
