9eb0512b30f87cf98adcea4d3e8dff75f9021b34
AkshayN01 <nambly77@gmail.com>
2024-11-07 18:19:42 +0000

    Added DB model for Notification Service

---
+++ src/CKMS/CKMS.Contracts/DBModels/NotificationService/Notification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CKMS.Contracts.DBModels.NotificationService
+{
+    public class Notification
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public int UserType { get; set; }
+        public Guid UserId { get; set; }
+        public String Title { get; set; }
+        public String Description { get; set; }
+        public int IsSent { get; set; }
+        public DateTime DateTime {  get; set; }
+    }
+}
