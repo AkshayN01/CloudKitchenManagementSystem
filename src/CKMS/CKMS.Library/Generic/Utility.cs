0cf5f0d9be3db3f15159eb1ce557b0d2f5b936b0
AkshayN01 <nambly77@gmail.com>
2024-11-27 12:54:15 +0000

    Added logic for Serialise and deserialse data

--- src/CKMS/CKMS.Library/Generic/Utility.cs
+++ src/CKMS/CKMS.Library/Generic/Utility.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -44,6 +45,35 @@ namespace CKMS.Library.Generic
         public static bool IsValueInEnum<TEnum>(int value) where TEnum : Enum
         {
             return Enum.IsDefined(typeof(TEnum), value);
+        }
+        public static async Task<String> SerialiseData<T>(T data)
+        {
+            String json = String.Empty;
+
+            using (var stream = new MemoryStream())
+            {
+                await JsonSerializer.SerializeAsync<T>(stream, data);
+                stream.Position = 0; // Reset the stream position to read the output
+
+                using (var reader = new StreamReader(stream))
+                {
+                    json = await reader.ReadToEndAsync();
+                    Console.WriteLine(json);
+                }
+            }
+
+            return json;
+        }
+        public static async Task<T?> DeserialiseData<T>(String json)
+        {
+            T? data = default(T);
+
+            using (var stream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(json)))
+            {
+                data = await JsonSerializer.DeserializeAsync<T>(stream);
+            }
+
+            return data;
         }
     }
 }
