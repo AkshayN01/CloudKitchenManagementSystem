c12dc9bf42b3fd46a88169ecf72c25f5a1a462ac
AkshayN01 <nambly77@gmail.com>
2024-12-03 19:23:44 +0000

    Added migration for Admin service

---
+++ src/CKMS/backend/AdminUserService/CKMS.AdminUserService.DataAccess/Migrations/20241129192016_InitialCreate.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CKMS.AdminUserService.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Kitchens",
+                columns: table => new
+                {
+                    KitchenId = table.Column<Guid>(type: "uuid", nullable: false),
+                    KitchenName = table.Column<string>(type: "text", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    City = table.Column<string>(type: "text", nullable: false),
+                    Region = table.Column<string>(type: "text", nullable: false),
+                    PostalCode = table.Column<string>(type: "text", nullable: false),
+                    Country = table.Column<string>(type: "text", nullable: false),
+                    EmailId = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    LastUpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kitchens", x => x.KitchenId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AdminUsers",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserName = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: false),
+                    EmailId = table.Column<string>(type: "text", nullable: false),
+                    FullName = table.Column<string>(type: "text", nullable: false),
+                    RoleId = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    LastUpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    LastLogin = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsEmailVerified = table.Column<int>(type: "integer", nullable: false),
+                    VerificationToken = table.Column<string>(type: "text", nullable: true),
+                    KitchenId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AdminUsers", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_AdminUsers_Kitchens_KitchenId",
+                        column: x => x.KitchenId,
+                        principalTable: "Kitchens",
+                        principalColumn: "KitchenId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AdminUsers_KitchenId",
+                table: "AdminUsers",
+                column: "KitchenId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AdminUsers_UserName",
+                table: "AdminUsers",
+                column: "UserName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Kitchens_EmailId",
+                table: "Kitchens",
+                column: "EmailId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AdminUsers");
+
+            migrationBuilder.DropTable(
+                name: "Kitchens");
+        }
+    }
+}
