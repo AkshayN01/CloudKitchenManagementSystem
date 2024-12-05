4a0fc5ac2cbd089b4a725c6b8c93ac02f544923e
AkshayN01 <nambly77@gmail.com>
2024-12-05 19:06:51 +0000

    Added new 'IsActive'  col in Kitchen table

---
+++ src/CKMS/backend/AdminUserService/CKMS.AdminUserService.DataAccess/Migrations/20241205190540_Added IsActive Column.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CKMS.AdminUserService.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedIsActiveColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "IsActive",
+                table: "Kitchens",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Kitchens");
+        }
+    }
+}
