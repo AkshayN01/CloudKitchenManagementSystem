9b30dd4de36ba6ce6bfc1b8eab591548ad35471d
AkshayN01 <nambly77@gmail.com>
2024-12-05 22:07:30 +0000

    Added logic to seed menu item data in redis

---
+++ src/CKMS/CKMS.Library/SeedData/RedisSeed/RedisMenuItemSeed.cs
@@ -0,0 +1,30 @@
+﻿using CKMS.Contracts.DBModels.InventoryService;
+using CKMS.Library.SeedData.InventoryService;
+using StackExchange.Redis;
+
+namespace CKMS.Library.SeedData.RedisSeed
+{
+    public class RedisMenuItemSeed
+    {
+        IDatabase _redisDB;
+        public RedisMenuItemSeed(IConnectionMultiplexer connectionMultiplexer)
+        {
+            _redisDB = connectionMultiplexer.GetDatabase();
+        }
+        public async Task SeedDataAsync()
+        {
+            await SeedMenuItemAsync();
+        }
+
+        private async Task SeedMenuItemAsync()
+        {
+            List<MenuItem> menuItems = await MenuItemSeedData.GetMenuItems();
+            foreach (MenuItem menu in menuItems)
+            {
+                string keyName = $"kitchen:{menu.KitchenId}";
+                HashEntry[] hashEntries = new HashEntry[] { new HashEntry($"menu:{menu.MenuItemId}", $"{menu.Name}:{menu.Price}") };
+                await _redisDB.HashSetAsync(keyName, hashEntries);
+            }
+        }
+    }
+}
