327c95230f598c3ca707f7afde7bc4d221dbc1b4
AkshayN01 <nambly77@gmail.com>
2024-12-06 13:53:30 +0000

    Added Auto mapper profile for customer service

---
+++ src/CKMS/CKMS.Library/AMProfiles/CustomerServiceProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using CKMS.Contracts.DBModels.CustomerService;
+using CKMS.Contracts.DTOs.Customer.Response;
+
+namespace CKMS.Library.AMProfiles
+{
+    public class CustomerServiceProfile : Profile
+    {
+        public CustomerServiceProfile() 
+        {
+            CreateMap<Customer, CustomerDTO>()
+                .ForMember(dest => dest.CustomerId, src => src.MapFrom(x => x.CustomerId))
+                .ForMember(dest => dest.PhoneNumber, src => src.MapFrom(x => x.PhoneNumber))
+                .ForMember(dest => dest.EmailId, src => src.MapFrom(x => x.EmailId))
+                .ForMember(dest => dest.UserName, src => src.MapFrom(x => x.UserName))
+                .ForMember(dest => dest.LoyaltyPoints, src => src.MapFrom(x => x.LoyaltyPoints))
+                .ForMember(dest => dest.Name, src => src.MapFrom(x => x.Name))
+                .ForMember(dest => dest.TotalOrder, src => src.MapFrom(x => x.TotalOrder));
+
+            CreateMap<Address, AddressDTO>()
+                .ForMember(dest => dest.AddressId, src => src.MapFrom(x => x.AddressId))
+                .ForMember(dest => dest.AddressDetail, src => src.MapFrom(x => x.AddressDetail))
+                .ForMember(dest => dest.PostalCode, src => src.MapFrom(x => x.PostalCode))
+                .ForMember(dest => dest.Region, src => src.MapFrom(x => x.Region))
+                .ForMember(dest => dest.City, src => src.MapFrom(x => x.City))
+                .ForMember(dest => dest.Country, src => src.MapFrom(x => x.Country));
+        }
+    }
+}
