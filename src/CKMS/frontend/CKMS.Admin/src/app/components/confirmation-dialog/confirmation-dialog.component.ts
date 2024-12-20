94a6a97596e6e09c4021c2140b96fb51db4d3f4a
AkshayN01 <nambly77@gmail.com>
2024-12-20 22:14:14 +0000

    Added confirmation modal component

---
+++ src/CKMS/frontend/CKMS.Admin/src/app/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -0,0 +1,19 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatDialog, MatDialogActions, MatDialogClose, MatDialogContent } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  templateUrl: './confirmation-dialog.component.html',
+  styleUrl: './confirmation-dialog.component.css'
+})
+export class ConfirmationDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmationDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { title:string, message: string }
+  ) {}
+
+  onSelect(result: boolean): void {
+    this.dialogRef.close(result);
+  }
+}
