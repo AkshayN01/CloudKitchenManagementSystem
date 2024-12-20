57690289f60e0f262f14ef80e7e75a44f49eff75
AkshayN01 <nambly77@gmail.com>
2024-12-20 22:14:53 +0000

    Updated register component

--- src/CKMS/frontend/CKMS.Admin/src/app/components/register/register.component.ts
+++ src/CKMS/frontend/CKMS.Admin/src/app/components/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserAccountService } from '../../services/user-account/user-account.service';
+import { Router } from '@angular/router';
+import { UtilityService } from '../../services/utility/utility.service';
 
 @Component({
   selector: 'app-register',
@@ -9,7 +12,7 @@ import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/fo
 export class RegisterComponent {
   registerForm: FormGroup;
   
-  constructor(private fb : FormBuilder){
+  constructor(private fb : FormBuilder, private userAccountService: UserAccountService, private router: Router, private utilityService: UtilityService){
     this.registerForm = this.fb.group({
       kitchenName: ['', Validators.required],
       address: ['', Validators.required],
@@ -37,7 +40,16 @@ export class RegisterComponent {
   }
 
   onSubmit(){
-
+    if(!this.registerForm.invalid){
+      this.userAccountService.registerKitchen(this.registerForm.value).subscribe((res) => {
+        if(res){
+          this.utilityService.openSnackBar("A verification email will be sent to your email.");
+          setTimeout(()=>{
+            this.router.navigate(['/login']);
+          },3000);
+        }
+      })
+    }
   }
   
   resetForm() {
