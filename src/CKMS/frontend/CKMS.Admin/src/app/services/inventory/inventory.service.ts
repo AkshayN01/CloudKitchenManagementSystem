5550896d8267bc7a04657aa84b940bb0d5a53312
AkshayN01 <nambly77@gmail.com>
2025-01-02 04:30:45 +0000

    Added APIs for inventory movement

--- src/CKMS/frontend/CKMS.Admin/src/app/services/inventory/inventory.service.ts
+++ src/CKMS/frontend/CKMS.Admin/src/app/services/inventory/inventory.service.ts
@@ -4,6 +4,8 @@ import { environment } from '../../../environments/environment';
 import { InventoryPayload, InventoryUpdatePayload } from '../../models/request/inventory/inventory';
 import { Observable } from 'rxjs';
 import { InventoryDTO, InventoryListDTO } from '../../models/response/inventory/inventory';
+import { InventoryMovementPayload, InventoryMovementUpdatePayload } from '../../models/request/inventory/inventoryMovement';
+import { InventoryMovementDTO, InventoryMovementListDTO } from '../../models/response/inventory/inventoryMovement';
 
 @Injectable({
   providedIn: 'root'
@@ -37,5 +39,31 @@ export class InventoryService {
   getAllInventory(pageSize: number, pageNumber: number): Observable<InventoryListDTO>{
     var apiUrl = `${this.baseUrl}/api/inventory/get-all-inventory?pageSize=${pageSize}&pageNumber=${pageNumber}`;
     return this.apiService.getData<InventoryListDTO>(apiUrl);
+  }
+
+  //inventory movement
+  addInventoryMovement(payload: InventoryMovementPayload): Observable<boolean>{
+    var apiUrl = `${this.baseUrl}/api/inventory/add-inventory-movement`;
+    return this.apiService.postData<boolean, InventoryMovementPayload>(apiUrl, payload);
+  }
+
+  updateInventoryMovement(payload: InventoryMovementUpdatePayload): Observable<boolean>{
+    var apiUrl = `${this.baseUrl}/api/inventory/update-inventory-movement`;
+    return this.apiService.postData<boolean, InventoryMovementUpdatePayload>(apiUrl, payload);
+  }
+
+  deleteInventoryMovement(inventoryMovementId: number): Observable<boolean>{
+    var apiUrl = `${this.baseUrl}/api/inventory/delete-inventory-movement/${inventoryMovementId}`;
+    return this.apiService.deleteData<boolean>(apiUrl);
+  }
+
+  getInventoryMovement(inventoryId: number): Observable<InventoryMovementDTO>{
+    var apiUrl = `${this.baseUrl}/api/inventory/get-inventory-movement/${inventoryId}`;
+    return this.apiService.getData<InventoryMovementDTO>(apiUrl);
+  }
+
+  getAllInventoryMovement(inventoryId: number, pageSize: number, pageNumber: number): Observable<InventoryMovementListDTO>{
+    var apiUrl = `${this.baseUrl}/api/inventory/get-all-inventory-movement/${inventoryId}?pageSize=${pageSize}&pageNumber=${pageNumber}`;
+    return this.apiService.getData<InventoryMovementListDTO>(apiUrl);
   }
 }
