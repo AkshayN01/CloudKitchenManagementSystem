cbb781f519d63f5c682f95018beb1f5f6925a931
AkshayN01 <nambly77@gmail.com>
2025-01-02 04:42:15 +0000

    Updated variable values

--- src/CKMS/frontend/CKMS.Customer/src/environments/environment.ts
+++ src/CKMS/frontend/CKMS.Customer/src/environments/environment.ts
@@ -1,10 +1,11 @@
 export const environment = {
+    adminServiceDomain: "https://localhost:7229",
     orderServiceDomain: "https://localhost:7009",
-    customerServiceDomain: "https://localhost:7197",
+    customerServiceDomain: "https://localhost:7048",
     inventoryServiceDomain: "https://localhost:7197",
     notificationServiceDomain: "https://localhost:7250",
     authStorageName: "authToken",
     loginAPIUrl: "https://localhost:7229/api/admin/login",
     registerAPIUrl: "https://localhost:7229/api/admin/register",
-    notificationCustomerUrl: ""
+    notificationCustomerUrl: "https://localhost:7250/customer-notification-hub"
 };
