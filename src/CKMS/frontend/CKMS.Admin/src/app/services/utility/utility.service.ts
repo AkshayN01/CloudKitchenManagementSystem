7426f70afc743253e17962c1a2c1f8566d6da53c
AkshayN01 <nambly77@gmail.com>
2025-01-02 04:29:09 +0000

    Added logic to convert date

--- src/CKMS/frontend/CKMS.Admin/src/app/services/utility/utility.service.ts
+++ src/CKMS/frontend/CKMS.Admin/src/app/services/utility/utility.service.ts
@@ -11,4 +11,22 @@ export class UtilityService {
   openSnackBar(message: string) {
     this.matSnackBar.open(message, 'Dismiss', { duration: 3000 });
   }
+
+  convertDate(dateString: string): string {
+
+    // Create a new Date object
+    const date = new Date(dateString);
+
+    // Get the day, month, and year components
+    const day = ("0" + date.getDate()).slice(-2); // Zero padding
+    const month = ("0" + (date.getMonth() + 1)).slice(-2); // Months are zero-based
+    const year = date.getFullYear();
+
+    // Format the date in the desired format
+    const formattedDate = `${day}/${month}/${year} 00:00:00`;
+
+    console.log(formattedDate); // Output: "01/04/2024 00:00:00"
+
+    return formattedDate;
+  }
 }
